b0642e189317a46575cb5d6baa7fcf163d4eeb11
AlejandroDeveloper <alejandro.palomo@external.grifols.com>
2018-01-18 12:09:15 +0100

    Initial Commit

---
+++ PrivaliaTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivaliaTest
+//
+//  Created by Alejandro Palomo Rodriguez on 18/1/18.
+//  Copyright © 2018 Alejandro Palomo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
